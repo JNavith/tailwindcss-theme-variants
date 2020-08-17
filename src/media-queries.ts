b27410763d03b2ab94801f750592ede5a59cbf2d
Navith <navithnavithnavith+git@gmail.com>
2020-08-17 14:39:37 -0400

    ⚡️ feat: can hover / can't hover media queries

--- src/media-queries.ts
+++ src/media-queries.ts
@@ -10,3 +10,6 @@ export const prefersReducedTransparency = "@media (prefers-reduced-transparency:
 export const prefersAnyContrast = "@media (prefers-contrast: no-preference)";
 export const prefersHighContrast = "@media (prefers-contrast: high)";
 export const prefersLowContrast = "@media (prefers-contrast: low)";
+
+export const canHover = "@media (hover: hover)";
+export const noHover = "@media (hover: none)";
