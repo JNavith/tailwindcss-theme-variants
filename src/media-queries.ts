d9826b6a8166528b66cd1b59bde24d5af8ad735d
Navith <navithnavithnavith+git@gmail.com>
2020-08-17 15:02:11 -0400

    ⚡️ feat: more media queries

--- src/media-queries.ts
+++ src/media-queries.ts
@@ -11,5 +11,18 @@ export const prefersAnyContrast = "@media (prefers-contrast: no-preference)";
 export const prefersHighContrast = "@media (prefers-contrast: high)";
 export const prefersLowContrast = "@media (prefers-contrast: low)";
 
+export const colorsInverted = "@media (inverted-colors: inverted)";
+export const colorsNotInverted = "@media (inverted-colors: none)";
+
 export const canHover = "@media (hover: hover)";
 export const noHover = "@media (hover: none)";
+
+export const canAnyHover = "@media (any-hover: hover)";
+export const noAnyHover = "@media (any-hover: none)";
+
+export const landscape = "@media (orientation: landscape)";
+export const portrait = "@media (orientation: portrait)";
+
+export const print = "@media print";
+export const screen = "@media screen";
+export const speech = "@media speech";
