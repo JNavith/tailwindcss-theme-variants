af21985891cfaf13808b0b89b0869aa5bae72e40
Navith <navithnavithnavith+git@gmail.com>
2020-09-23 13:14:31 -0400

    🎨 style: icons for themes on the documentation site

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -3,6 +3,10 @@
 	import Icon from "@iconify/svelte";
 	import github from "@iconify/icons-mdi/github";
 	import npm from "@iconify/icons-mdi/npm-variant";
+	import colorSwatch from "@iconify/icons-heroicons-solid/color-swatch";
+	import moon from "@iconify/icons-heroicons-solid/moon";
+	import sun from "@iconify/icons-heroicons-solid/sun";
+	import chip from "@iconify/icons-heroicons-solid/chip";
 
 	import Metadata from "../components/Metadata.svelte";
 
@@ -17,6 +21,12 @@
 	if (process.browser) {
 		themeOptions = ["light", "dark"]
 		if (matchMedia("(prefers-color-scheme: light)").matches || matchMedia("(prefers-color-scheme: dark)").matches) themeOptions = [...themeOptions, "system"];
+	}
+
+	const themeIcons = {
+		light: sun,
+		dark: moon,
+		system: colorSwatch,
 	}
 
 	$: if (process.browser) {
@@ -38,14 +48,14 @@
 		<!-- Theme selection -->
 		<div class="px-5 text-on-primary-faint-100">
 			{#if themeOptions.length > 0}
-				<span id="theme-label">Theme:</span>
+				<span id="theme-label" class="sr-only md:not-sr-only">Theme:</span>
 			{/if}
 			{#each themeOptions as themeOption}
 				<div class="inline-block">
 					<button aria-labelledby="theme-label" class="px-2 py-2 sm:py-5 group" on:click={() => $theme = themeOption}>
-						<span class="p-1 -m-1 rounded transition-theme {$theme === themeOption ? 'font-semibold bg-primary-faint-200' : 'group-hocus:bg-primary-faint-200'}">
-							{themeOption.slice(0, 1).toUpperCase()}{themeOption.slice(1)}
-						</span>
+						<div class="p-1 -m-1 rounded transition-theme {$theme === themeOption ? 'font-semibold bg-primary-faint-200' : 'group-hocus:bg-primary-faint-200'}">
+							<Icon icon={themeIcons[themeOption]} style="font-size: 125%" class="inline align-middle transition-theme {$theme === themeOption ? '' : 'text-on-primary-faint-300 group-hocus:text-on-primary'}" /><span class="lg:pl-1 sr-only lg:not-sr-only">{themeOption.slice(0, 1).toUpperCase() + themeOption.slice(1)}</span>
+						</div>
 					</button>
 				</div>
 			{/each}
@@ -55,7 +65,7 @@
 		<!-- External links for the package -->
 		<div class="pr-5">
 			{#each [[github, "GitHub", "https://github.com/JakeNavith/tailwindcss-theme-variants"], [npm, "NPM", "https://www.npmjs.com/package/tailwindcss-theme-variants"]] as [icon, name, link]}
-				<a title={name} class="p-2 pr-0 lg:p-5 lg:pr-5 group text-on-primary-faint-200 hocus:text-on-primary transition-theme" href={link}><Icon {icon} style="font-size: 150%" class="inline align-middle text-on-primary-faint-300 group-hocus:text-on-primary transition-theme" /><span class="pl-1 align-middle group-hocus:underline hidden md:inline">{name}</span></a>
+				<a title={name} class="p-2 pr-0 lg:p-5 lg:pr-5 group text-on-primary-faint-200 hocus:text-on-primary transition-theme" href={link}><Icon {icon} style="font-size: 125%" class="inline align-middle text-on-primary-faint-300 group-hocus:text-on-primary transition-theme" /><span class="md:pl-1 align-middle group-hocus:underline sr-only md:not-sr-only">{name}</span></a>
 			{/each}
 		</div>
 		<!-- End external links for the package -->
